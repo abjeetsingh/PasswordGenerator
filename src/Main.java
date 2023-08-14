ad211a82bb7b0a6399ac332378bd4c4187c2883a
Abjeet Singh <abjeets50@gmail.com>
2023-08-14 06:59:07 +0530

    feat: user values

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the length of the password: ");
+        int length = input.nextInt();
+
+        System.out.print("Enter the complexity of the password (1-3): ");
+        int complexity = input.nextInt();
+
+        String password = generatePassword(length, complexity);
+        System.out.println("Your password is: " + password);
     }
 }
