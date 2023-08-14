b193a5ad8e2643421e9cbfa3d5628a8ea9f6881b
Abjeet Singh <abjeets50@gmail.com>
2023-08-14 07:02:54 +0530

    feat: add generate function

--- src/Main.java
+++ src/Main.java
@@ -13,4 +13,29 @@ public class Main {
         String password = generatePassword(length, complexity);
         System.out.println("Your password is: " + password);
     }
+
+    public static String generatePassword(int length, int complexity) {
+        Random rand = new Random();
+        String lowercase = "abcdefghijklmnopqrstuvwxyz";
+        String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String numbers = "0123456789";
+        String symbols = "!@#$%^&*()_+-=";
+
+        String characters = lowercase;
+        if (complexity >= 2) {
+            characters += uppercase;
+        }
+        if (complexity >= 3) {
+            characters += numbers + symbols;
+        }
+
+        StringBuilder password = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int index = rand.nextInt(characters.length());
+            password.append(characters.charAt(index));
+        }
+
+        return password.toString();
+    }
+
 }
